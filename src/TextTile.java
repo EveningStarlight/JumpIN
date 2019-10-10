d54feae1adf164f9676eebdf89f67d2787319cb7
mattman555 <1999mrh@gmail.com>
2019-10-10 13:19:33 -0400

    Fixed UML for TextTile and added methods to TextTile

--- src/TextTile.java
+++ src/TextTile.java
@@ -1,8 +1,29 @@
-
+/**
+ * 
+ * @author Matthew Harris
+ * 			101 073 502
+ * @version 1.0.2
+ * 
+ *  This class holds coordinates for other objects
+ */
 public class TextTile {
 	
 	public final Coord coord;
-	public TextTile(){
-		coord = new coord();
+	private Piece piece;
+	
+	public TextTile(Coord coord, Piece piece){
+		this.coord = coord;
+		this.piece = piece;
+	}
+	
+	public Piece getPiece(){
+		return this.piece;
+	}
+	
+	public void setPiece(Piece piece){
+		this.piece = piece;
+	}
+	public boolean isEmpty(){
+		return true;
 	}
 }
