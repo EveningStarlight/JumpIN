850969af98a75e20cde40779e9da9d244c32cd02
adam Prins <AdamPrins@cmail.carleton.ca>
2019-10-10 14:56:02 -0400

    Game Implementation
    Basic code for all game functions add
    Comments for everything

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,15 @@
 import java.util.ArrayList;
 import java.util.Stack;
 
+/**
+ * Overall game implementation 
+ * contains the board and is in charge of swapping pieces
+ * @author Adam Prins
+ * 			100 879 683
+ * @version 1.0.0
+ * 		First implementation of the code
+ * 		Basic usage of all functions in the UML
+ */
 public class Game {
 	public static final int BOARD_SIZE=5;
 	private Tile board[][];
@@ -9,6 +18,10 @@ public class Game {
 	private Stack<Move> redoStack;
 	
 	/**
+	 * sets up the game
+	 * populates the board with coordinates
+	 * fetches the puzzleArray
+	 * calls setBoard to populate the pieces using the puzzleArray
 	 * 
 	 * @param puzzleNum the puzzle that is to be initialized
 	 */
@@ -23,9 +36,19 @@ public class Game {
 				board[x][y]=new TextTile(new Coord(x,y));
 			}
 		}
+		
+		//TODO use puzzleNum to populate board with pieces
+		//setBoard();
 	}
 	
 	/**
+	 * used for playing the game
+	 * 
+	 * selects a tile if there is not currently a selected tile
+	 * deselects the tile if it was already the selected tile
+	 * Otherwise, it tries to swap the tiles
+	 * 
+	 * TODO GUI will have buttons not be selectable if mushroom or other conditions
 	 * 
 	 * @param coord the newly selected tile.
 	 */
@@ -34,7 +57,7 @@ public class Game {
 			tileSelected=this.getTile(coord);
 		}
 		else if (tileSelected==this.getTile(coord)){
-			
+			tileSelected=null;
 		}
 		else {
 			try {
@@ -46,6 +69,7 @@ public class Game {
 	}
 	
 	/**
+	 * gets the tile with the given coordinates 
 	 * 
 	 * @param coord the coordinates of the tile you are trying to acquire
 	 * @return the tile found at the given coordinates
@@ -55,15 +79,17 @@ public class Game {
 	}
 	
 	/**
+	 * Populates the board with pieces as described in the passed array
 	 * 
-	 * @param boardArr
+	 * @param boardArr describes how to populate the board
 	 */
 	public void setBoard(ArrayList<Piece> boardArr) {
+		clearBoard();
 		//TODO implement
 	}
 	
 	/**
-	 * 
+	 * undoes the last move made as described on the undoStack
 	 */
 	public void undoMove() {
 		if (undoStack.isEmpty()) {
@@ -71,7 +97,7 @@ public class Game {
 		}
 		else {
 			Move move = undoStack.pop();
-			this.getTile(move.coordOld).setPiece(this.getTile(move.coordNew.removePiece()));
+			this.getTile(move.coordOld).setPiece(this.getTile(move.coordNew).removePiece());
 			redoStack.add(move);
 			if (undoStack.isEmpty()) {
 				//TODO disable undo button
@@ -80,7 +106,7 @@ public class Game {
 	}
 	
 	/**
-	 * 
+	 * redoes the next move made as described on the redoStack
 	 */
 	public void redoMove() {
 		if (redoStack.isEmpty()) {
@@ -88,7 +114,7 @@ public class Game {
 		}
 		else {
 			Move move = redoStack.pop();
-			this.getTile(move.coordNew).setPiece(this.getTile(move.coordOld.removePiece()));
+			this.getTile(move.coordNew).setPiece(this.getTile(move.coordOld).removePiece());
 			undoStack.add(move);
 			if (redoStack.isEmpty()) {
 				//TODO disable redo button
@@ -97,14 +123,29 @@ public class Game {
 	}
 	
 	/**
+	 * Attempts to swap two pieces
+	 * bunnies need to jump over filled spaces,
+	 * foxes need to side though empty spaces
+	 * mushrooms don't move
 	 * 
-	 * @param coord 
+	 * @param coord destination of piece stored at TileSelected
 	 */
 	private void swapPiece(Coord coord) {
 		
 		
 		if (this.getTile(coord).getPiece().isValidMove()) {
-			undoStack.add(new Move(tileSelected.coord,coord));
+			undoStack.add(new Move(tileSelected.getCoord(),coord));
+		}
+	}
+	
+	/**
+	 * removes all Pieces from the board
+	 */
+	private void clearBoard() {
+		for(Tile[] tileLine:board) {
+			for(Tile tile:tileLine) {
+				tile.removePiece();
+			}
 		}
 	}
 	
