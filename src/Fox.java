87d7aa2793b06906456dd3e74c71b43f189bd36c
Jay McCracken <32139643+jaymccracken@users.noreply.github.com>
2019-10-31 15:04:03 -0400

    Fixing more errors

--- src/Fox.java
+++ src/Fox.java
@@ -17,8 +17,8 @@ public class Fox extends Piece{
     private Coord tail;
 
     //the images to be placed on the buttons
-    public static final ImageIcon iconHead = new ImageIcon("ButtonIcons/fox_head.png");
-    public static final ImageIcon iconTail = new ImageIcon("ButtonIcons/fox_tail.png");
+    public static final ImageIcon iconHead = new ImageIcon("ButtonIcons/foxHead.png");
+    public static final ImageIcon iconTail = new ImageIcon("ButtonIcons/foxTail.png");
 
 
 
