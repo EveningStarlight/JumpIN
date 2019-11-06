54abe68b5850f1918391ef0988b84a4fee284f64
Jay McCracken <32139643+jaymccracken@users.noreply.github.com>
2019-11-06 14:09:47 -0500

    creation of the constructor and the base functions we want

--- src/Solver/Solver.java
+++ src/Solver/Solver.java
@@ -1,6 +1,9 @@
 package Solver;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import GUI.ButtonTile;
 import GUI.Tile;
@@ -12,16 +15,34 @@ import Pieces.Piece;
  * The solver for the puzzles, with hints and how to solve each puzzle
  * @authors Jay McCracken, Matthew Harris
  * 			101066860       101073502
+ * @version 1.0.1
+ * 	constructor created
  */
 public class Solver {
 
 	private ArrayList<Piece> pieces;
+	private HashMap<Piece, Boolean> visitedPieces;
 	private Game game;
 	private Tile[][] board;
+	private Queue<String> movesTaken;
 	
 	public Solver(int puzzleNumber) throws Exception{
 		pieces = new ArrayList<Piece>(Puzzles.getPuzzle(puzzleNumber));
+		visitedPieces = new HashMap<Piece, Boolean>();
+		movesTaken = new LinkedList<String>();
 		game = new Game(board, puzzleNumber);
 		}
 	
+	
+	private void bunnyDepthSearch() {
+		
+	}
+	
+	public void puzzleDepthSearch() {
+		
+	}
+	
+	public String toString() {
+		return null;
+	}
 }
