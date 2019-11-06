4e0d503c4d89f20c72478d89d502a7016769038f
mattman555 <1999mrh@gmail.com>
2019-11-06 13:35:04 -0500

    Solver Constructor

--- src/Solver/Solver.java
+++ src/Solver/Solver.java
@@ -1,12 +1,30 @@
 package Solver;
+
+import java.util.ArrayList;
+
+import GUI.ButtonTile;
+import GUI.Tile;
+import Model.Game;
+import Model.Puzzles;
+import Pieces.Piece;
+
 /**
  * The solver for the puzzles
- * @authors Jay McCracken 
- * 			101066860
+ * @authors Jay McCracken, Matthew Harris
+ * 			101066860       101073502
  * @version 1.0.0
  * 		
  * 
  */
 public class Solver {
 
+	private ArrayList<Piece> pieces;
+	private Game game;
+	private Tile[][] board;
+	
+	public Solver(int puzzleNumber) throws Exception{
+		pieces = new ArrayList<Piece>(Puzzles.getPuzzle(puzzleNumber));
+		game = new Game(board, puzzleNumber);
+		}
+	
 }
