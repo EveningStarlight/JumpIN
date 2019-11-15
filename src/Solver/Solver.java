79224358e4140148ba9ac3258d4da9ba778a7d3a
mattman555 <1999mrh@gmail.com>
2019-11-15 15:21:46 -0500

    further solver implementation

--- src/Solver/Solver.java
+++ src/Solver/Solver.java
@@ -25,16 +25,16 @@ import Pieces.Piece;
 public class Solver {
 
 	private ArrayList<Piece> pieces;
-	private HashMap<Piece, Boolean> visitedPieces;
+	private ArrayList<Piece> visitedPieces;
 	private Game solverGame;
 	private Tile[][] board;
 	private Queue<String> movesTaken;
 	
 	public Solver(int puzzleNumber) throws Exception{
 		pieces = new ArrayList<Piece>(Puzzles.getPuzzle(puzzleNumber));
-		visitedPieces = new HashMap<Piece, Boolean>();
+		visitedPieces = new ArrayList<Piece>();
 		for(int i = 0; i<pieces.size();i++){
-			visitedPieces.put(pieces.get(i),false);
+			visitedPieces.add(pieces.get(i));
 		}
 		movesTaken = new LinkedList<String>();
 		solverGame = new Game(board, puzzleNumber);
@@ -42,9 +42,9 @@ public class Solver {
 	
 	public Solver(Game game) throws Exception{
 		pieces = new ArrayList<Piece>(game.getBoard());
-		visitedPieces = new HashMap<Piece, Boolean>();
+		visitedPieces = new ArrayList<Piece>();
 		for(int i = 0; i<pieces.size();i++){
-			visitedPieces.put(pieces.get(i),false);
+			visitedPieces.add(pieces.get(i));
 		}
 		movesTaken = new LinkedList<String>();
 		solverGame = new Game(board, 0 );
@@ -52,39 +52,41 @@ public class Solver {
 		}
 	
 	
-	public void puzzleBreadthSearch() {
-		visitedPieces.replace(pieces.get(0), true);
-		ArrayList<Move> moves;
-		for(int i =0; i< visitedPieces.size(); i++) {
-			moves = avaliableMoves((Piece) visitedPieces.keySet().toArray()[i]);
-			while(!moves.isEmpty()){
-				if(solverGame.endGame()){
-					break;
-				}
-			}	
-		}
-		
+	public ArrayList<Move> puzzleBreadthSearch() throws Exception {
+		ArrayList<ArrayList<Move>> superMoves = new ArrayList<ArrayList<Move>>();
+		superMoves.add(new ArrayList<Move>());
+		int i=0;
+		do {
+			doAllMoves(superMoves.get(i));
+			ArrayList<Move> moves=avaliableMoves(pieces.get(i));
+			for (int j=0; j<moves.size(); j++) {
+				ArrayList<Move> currMove=(ArrayList<Move>) superMoves.get(i).clone();
+				currMove.add(moves.get(j));
+				superMoves.add(currMove);
+			}
+		i++;
+		}while (i<superMoves.size());
+		return null;
 	}
 	
-	public ArrayList<Move> avaliableMoves(Piece piece) {
+	private void doAllMoves(ArrayList<Move> arrayList) {
+		
+	}
+
+	public ArrayList<Move> avaliableMoves() {
 		ArrayList<Move> moves = new ArrayList<Move>();
-			for(int i = 1; i < 5;i++) {
-				Coord left = new Coord((piece.getCoord().x-i), piece.getCoord().y);
-				Coord right = new Coord((piece.getCoord().x+i), piece.getCoord().y);
-				Coord up = new Coord((piece.getCoord().x), piece.getCoord().y-i);
-				Coord down = new Coord((piece.getCoord().x), piece.getCoord().y+i);
-				if(piece.isValidMove(left)) {
+		for(int i = 0; i<pieces.size();i++){
+			Piece piece = pieces.get(i);
+			for(int j = 0; j < 5;j++) {
+				Coord x = new Coord(j, piece.getCoord().y);
+				Coord y = new Coord(piece.getCoord().x, j);
+				if() {
 					moves.add(new Move(piece.getCoord(),left));
 				}
 				if(piece.isValidMove(right)) {
 					moves.add(new Move(piece.getCoord(),right));
 				}
-				if(piece.isValidMove(up)) {
-					moves.add(new Move(piece.getCoord(),up));
-				}
-				if(piece.isValidMove(down)) {
-					moves.add(new Move(piece.getCoord(),down));
-				}
+			}
 			}
 		return moves;	
 	}
