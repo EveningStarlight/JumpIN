f9a2e3fc3550517d9f66277049669e663dc54144
mattman555 <1999mrh@gmail.com>
2019-11-18 17:15:51 -0500

    made the level builder class to save user created puzzles to the xml of puzzles

---
+++ src/Model/LevelBuilder.java
@@ -0,0 +1,63 @@
+package Model;
+
+import java.util.ArrayList;
+import Pieces.*;
+import Solver.Solver;
+import java.util.*;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.*;
+import javax.xml.transform.dom.*;
+import org.w3c.dom.*;
+
+import javax.xml.parsers.*;
+
+/**
+ * The level builder it writes a possible level to the puzzles xml 
+ * @author Matthew Harris
+ * 			101073502
+ */
+public class LevelBuilder{
+	
+	ArrayList<Piece> pieces = new ArrayList<Piece>();
+	
+	public LevelBuilder(ArrayList<Piece> pieces){
+		this.pieces=pieces;
+	}
+	
+	private boolean isSolvable() throws Exception{
+		Solver solution = new Solver(pieces);
+		if(solution.puzzleBreadthSearch()==null){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	public boolean save() throws Exception{
+		if(isSolvable()){
+			try{
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		    DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		    Document document = documentBuilder.parse("Puzzles.xml");
+		    Element root = document.getDocumentElement();
+	        Element puzzleRootElement = document.createElement("Puzzle");
+	        Element numberElement = document.createElement("Number");
+	        numberElement.appendChild(document.createTextNode(Integer.toString(Puzzles.getMaxPuzzle()+1)));
+	        puzzleRootElement.appendChild(numberElement);
+	        for (Piece piece:pieces){
+	        	puzzleRootElement.appendChild(piece.getElement(document));
+	        }
+	        	return true;
+			}
+			catch(Exception e){
+				return false;
+			}
+		}
+		else{
+			return false;
+		}
+	}
+	
+
+}
